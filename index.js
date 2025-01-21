f0eab826f28906a09874b1a6d65bc3fb60c6114f
Animesh59 <abelieve0304@gmail.com>
2025-01-21 11:48:50 +0530

    done

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+app.get('/data', (req, res) => {
+    res.send('data')
 })
 
 app.listen(port, () => {
